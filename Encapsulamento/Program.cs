46c9641a9db304d7dabc3577e0ba6819dbb845fb
Emerson Marques Pedro <emerson.pedro@alumni.usp.br>
2024-03-28 08:15:18 -0300

    Corrigindo TiJela e Macbook

--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -7,7 +7,7 @@ namespace Encapsulamento
         static void Main(string[] args)
         {
             Produto p1 = new Produto("Tijela", 1.99);
-            Produto p2 = new Produto("Mc Book Wind", 999.90, 100);            
+            Produto p2 = new Produto("Macbook Wind", 999.90, 100);            
             Console.WriteLine(p1.Nome);
             p1.Nome = "Tappleware";
         }
