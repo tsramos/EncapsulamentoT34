efcbd6d9ce74e8bf32d95fcb965ee4cfcb4fd71c
Emerson Marques Pedro <emerson.pedro@alumni.usp.br>
2024-03-28 08:21:37 -0300

    Tupperware https://pt.wikipedia.org/wiki/Tupperware

--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -9,7 +9,7 @@ namespace Encapsulamento
             Produto p1 = new Produto("Tigela", 1.99);
             Produto p2 = new Produto("Macbook Wind", 999.90, 100);            
             Console.WriteLine(p1.Nome);
-            p1.Nome = "Tappleware";
+            p1.Nome = "Tupperware";
         }
     }
 }
